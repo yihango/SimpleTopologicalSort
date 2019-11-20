846dfbbacd32ab325e227e6300eaa2bd53da9280
staneee <yi.hang@live.com>
2019-11-20 17:05:19 +0800

    提交记录

---
+++ src/SimpleTopologicalSort/SimpleTopologicalSortDemo/DependModulesAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleTopologicalSortDemo
+{
+    /// <summary>
+    /// 依赖
+    /// </summary>
+    public class DependModulesAttribute : Attribute
+    {
+
+        public Type[] DependModuleTypes { get; private set; }
+
+        public DependModulesAttribute(params Type[] dependModuleTypes)
+        {
+            DependModuleTypes = dependModuleTypes;
+        }
+    }
+}
---
+++ src/SimpleTopologicalSort/SimpleTopologicalSortDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SimpleTopologicalSortDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://www.cnblogs.com/myzony/p/9201768.html
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
