cc77546d21e40e056e8ee6f5f374794a1fab2282
staneee <yi.hang@live.com>
2019-12-03 20:05:05 +0800

    修改代码

---
+++ src/EasyUnitOfWork/Database/IDatabaseResolver.cs
@@ -0,0 +1,18 @@
+﻿using JetBrains.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyUnitOfWork.Database
+{
+    public interface IDatabaseResolver
+    {
+        object GetService([NotNull]object key);
+
+        TValue GetService<TValue>([NotNull]object key)
+          where TValue : class, new();
+
+        TValue GetService<TKey, TValue>([NotNull]TKey key)
+            where TValue : class, new();
+    }
+}
