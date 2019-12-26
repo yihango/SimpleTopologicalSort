9917bb94d169edd7a2c6de42b27b7999feaa212d
stan e <yi.hang@live.com>
2019-12-26 20:13:05 +0800

    增加获取DbContext的函数

--- src/EasyUnitOfWork.EfCore/Uow/EfCoreUnitOfWork.cs
+++ src/EasyUnitOfWork.EfCore/Uow/EfCoreUnitOfWork.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Immutable;
+using EasyUnitOfWork.Extensions;
 
 namespace EasyUnitOfWork.Uow
 {
@@ -18,14 +19,69 @@ namespace EasyUnitOfWork.Uow
 
         private readonly IEfCoreTransactionStrategy _transactionStrategy;
 
+        private readonly IDbContextResolver _dbContextResolver;
+
         public EfCoreUnitOfWork(
             IServiceProvider serviceProvider,
-            IEfCoreTransactionStrategy transactionStrategy
+            IEfCoreTransactionStrategy transactionStrategy,
+            IDbContextResolver dbContextResolver
             )
         {
             this.ServiceProvider = serviceProvider;
             this._transactionStrategy = transactionStrategy;
             this.ActiveDbContexts = new Dictionary<string, DbContext>();
+            this._dbContextResolver = dbContextResolver;
+        }
+
+        /// <summary>
+        /// 获取或者创建数据库上下文
+        /// </summary>
+        /// <typeparam name="TDbContext">数据库上下文类型</typeparam>
+        /// <param name="name">名称</param>
+        /// <returns>数据库上下文对象</returns>
+        public virtual TDbContext GetOrCreateDbContext<TDbContext>(string name = null)
+           where TDbContext : DbContext
+        {
+            var dbContextType = typeof(TDbContext);
+
+            // TODO: 这里获取连接字符串,有待开发
+            var nameOrConnectionString = "Default";
+
+            var dbContextKey = $"{dbContextType.FullName}#{nameOrConnectionString}";
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                dbContextKey = $"{dbContextKey}#{name}";
+            }
+
+            DbContext dbContext;
+            if (!ActiveDbContexts.TryGetValue(dbContextKey, out dbContext))
+            {
+                if (Options.IsTransactional == true)
+                {
+                    dbContext = _transactionStrategy.CreateDbContext<TDbContext>(nameOrConnectionString, _dbContextResolver);
+                }
+                else
+                {
+                    dbContext = _dbContextResolver.Resolve<TDbContext>(nameOrConnectionString, null);
+                }
+
+                if (Options.Timeout.HasValue &&
+                    dbContext.Database.IsRelational() &&
+                    !dbContext.Database.GetCommandTimeout().HasValue)
+                {
+                    var commandTimeout = Convert.ToInt32(Options.Timeout.Value.TotalSeconds);
+
+                    //dbContext.Database.SetCommandTimeout(Options.Timeout.Value.TotalSeconds.To<int>());
+                    dbContext.Database.SetCommandTimeout(commandTimeout);
+                }
+
+                //TODO: Object materialize event
+                //TODO: Apply current filters to this dbcontext
+
+                ActiveDbContexts[dbContextKey] = dbContext;
+            }
+
+            return (TDbContext)dbContext;
         }
 
 
